8998c96b7c3e5091665311cbe4a237b20adc2172
Charlie McGregor <charlie_mcgregor@outlook.com>
2020-05-07 14:49:11 +1000

    Home page carousel images now redirect to relevant pages

--- frontend/src/components/landing/Carousel.js
+++ frontend/src/components/landing/Carousel.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import collection from "./graphics/jumbotron_collection.png";
@@ -6,13 +7,36 @@ import daytona from "./graphics/jumbotron_daytona.png";
 import finance from "./graphics/jumbotron_finance.png";
 import overseas from "./graphics/jumbotron_overseas.png";
 import patek from "./graphics/jumbotron_patek.png";
+import constants from "../../constants";
 
-const sources = [collection, daytona, finance, overseas, patek];
+const imageDetails = [
+  { src: collection, alt: "", route: constants.routes.UNDER_CONSTRUCTION },
+  //http://localhost:3000/product/Rolex/5ea64568541b50053c8a41db
+  {
+    src: daytona,
+    alt: "",
+    route: `${constants.routes.PRODUCT}/${constants.brands.ROLEX}/5ea64568541b50053c8a41db`,
+  },
+  { src: finance, alt: "", route: constants.routes.UNDER_CONSTRUCTION },
+  {
+    src: overseas,
+    alt: "",
+    route: `${constants.routes.BRANDS}/${constants.brands.VACHERON_CONSTANTIN}`,
+  },
+  {
+    src: patek,
+    alt: "",
+    route: `${constants.routes.BRANDS}/${constants.brands.PATEK_PHILIPPE}`,
+  },
+];
 
-const images = sources.map((img) => {
+const images = imageDetails.map((details, i) => {
   return (
-    <div>
-      <img alt="" src={img} />
+    <div
+      style={{ cursor: "pointer" }}
+      onClick={() => (window.location.href = details.route)}
+    >
+      <img alt={details.alt} src={details.src} />
     </div>
   );
 });
