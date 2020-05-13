9ee6e7f53219128e6e559eedfe0b0606d1160f12
Charlie McGregor <charlie_mcgregor@outlook.com>
2020-05-13 15:32:29 +1000

    Rewrote the Product page to use hooks instead of a class. Same with the ProductPreview.

--- frontend/src/components/product/Product.js
+++ frontend/src/components/product/Product.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Dots } from "react-activity";
 import "react-activity/dist/react-activity.css";
@@ -7,25 +7,20 @@ import constants from "../../constants";
 import styles from "./styles";
 import strings from "./strings";
 import ProductPreview from "../productPreview/ProductPreview";
-import zoom from "./graphics/zoom.png";
+import zoomImg from "./graphics/zoom.png";
 
-export default class Product extends Component {
-  constructor() {
-    super();
-    this.state = {
-      watch: {},
-      otherWatches: [],
-      zoom: false,
-    };
-  }
+export default function Product(props) {
+  const [watch, setWatch] = useState({});
+  const [otherWatches, setOtherWatches] = useState([]);
+  const [zoom, toggleZoom] = useState(false);
 
-  componentDidMount() {
-    const { params } = this.props.match;
+  useEffect(function () {
+    const { params } = props.match;
     axios
       .get(`${constants.api.WATCHES}/${params.brand}/${params._id}`)
       .then((res) => {
-        this.setState({ watch: res.data });
-        this.getOtherWatches();
+        setWatch(res.data);
+        getOtherWatches();
       })
       .catch((err) => {
         console.log(`Error getting the product: ${err}`);
@@ -35,19 +30,13 @@ export default class Product extends Component {
         //   payload: err.response.data,
         // });
       });
-  }
+  });
 
-  zoom = () => {
-    this.setState({
-      zoom: !this.state.zoom,
-    });
-  };
-
-  getOtherWatches = () => {
+  const getOtherWatches = () => {
     axios
-      .get(`${constants.api.WATCHES}/${this.state.watch.brand}`)
+      .get(`${constants.api.WATCHES}/${watch.brand}`)
       .then((res) => {
-        this.setState({ otherWatches: res.data });
+        setOtherWatches(res.data);
       })
       .catch((err) => {
         console.log(`error: ${err}`);
@@ -58,287 +47,115 @@ export default class Product extends Component {
       });
   };
 
-  render() {
-    const { watch } = this.state;
-    const watchImage = `data:image/jpeg;base64,${watch.image}`;
-    const otherWatches = this.state.otherWatches
-      .filter((product) => product.inStock)
-      .filter((product) => product._id != watch._id)
-      .map((watch, i) => {
-        return <ProductPreview watch={watch} />;
-      });
-    const activityIndicator = (
-      <div style={styles.activityIndicator}>
-        <Dots />
+  const watchImage = `data:image/jpeg;base64,${watch.image}`;
+  const otherWatchesRendered = otherWatches
+    .filter((product) => product.inStock)
+    .filter((product) => product._id !== watch._id)
+    .map((watch, i) => {
+      return <ProductPreview watch={watch} />;
+    });
+  const activityIndicator = (
+    <div style={styles.activityIndicator}>
+      <Dots />
+    </div>
+  );
+  const imageFullSize = (
+    <div style={styles.fullImageWrapper}>
+      {/* Change this to an actual icon instead of just text */}
+      <div style={styles.fullImageClose} onClick={() => toggleZoom(false)}>
+        x
       </div>
-    );
-    const imageFullSize = (
-      <div style={styles.fullImageWrapper}>
-        {/* Change this to an actual icon instead of just text */}
-        <div style={styles.fullImageClose} onClick={this.zoom}>
-          X
-        </div>
-        <img
-          alt={strings.accessibility.fullImage}
-          src={watchImage}
-          style={{ maxHeight: "100%" }}
-        />
-      </div>
-    );
-    const caseKey = watch.case && <p>{strings.case}</p>;
-    const braceletKey = watch.bracelet && <p>{strings.bracelet}</p>;
-    const dialKey = watch.dial && <p>{strings.dial}</p>;
-    const diameterKey = watch.diameter && <p>{strings.diameter}</p>;
-    const movementKey = watch.movement && <p>{strings.movement}</p>;
-    const complicationsKey = watch.complications !== undefined &&
-      watch.complications.length > 0 && <p>{strings.complications}</p>;
-    const complications =
-      watch.complications !== undefined &&
-      watch.complications.map((e, i) => {
-        return i > 0 ? `, ${e}` : e;
-      });
+      <img
+        alt={strings.accessibility.fullImage}
+        src={watchImage}
+        style={{ maxHeight: "100%" }}
+      />
+    </div>
+  );
+  const caseKey = watch.case && <p>{strings.case}</p>;
+  const braceletKey = watch.bracelet && <p>{strings.bracelet}</p>;
+  const dialKey = watch.dial && <p>{strings.dial}</p>;
+  const diameterKey = watch.diameter && <p>{strings.diameter}</p>;
+  const movementKey = watch.movement && <p>{strings.movement}</p>;
+  const complicationsKey = watch.complications !== undefined &&
+    watch.complications.length > 0 && <p>{strings.complications}</p>;
+  const complications =
+    watch.complications !== undefined &&
+    watch.complications.map((e, i) => {
+      return i > 0 ? `, ${e}` : e;
+    });
 
-    return (
-      <div style={styles.masterWrapper}>
-        {this.state.zoom && imageFullSize}
-        <div style={styles.topWrapper}>
-          <div style={styles.imageWrapper}>
-            <img
-              alt={strings.accessibility.productImage}
-              src={watchImage}
-              style={styles.image}
-              onClick={this.zoom}
-            />
-            <div style={styles.zoom}>
-              {strings.clickToZoom}
-              <img alt="" src={zoom} style={styles.zoomIcon} />
-            </div>
+  return (
+    <div style={styles.masterWrapper}>
+      {zoom && imageFullSize}
+      <div style={styles.topWrapper}>
+        <div style={styles.imageWrapper}>
+          <img
+            alt={strings.accessibility.productImage}
+            src={watchImage}
+            style={styles.image}
+            onClick={() => toggleZoom(true)}
+          />
+          <div style={styles.zoom}>
+            {strings.clickToZoom}
+            <img alt="" src={zoomImg} style={styles.zoomIcon} />
           </div>
-          <div style={styles.detailsWrapper}>
-            <div style={styles.title}>{watch.brand}</div>
-            <div style={styles.model}>{watch.model}</div>
-            <div style={styles.buyWrapper}>
-              <div style={styles.priceWrapper}>${watch.price}</div>
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
+        </div>
+        <div style={styles.detailsWrapper}>
+          <div style={styles.title}>{watch.brand}</div>
+          <div style={styles.model}>{watch.model}</div>
+          <div style={styles.buyWrapper}>
+            <div style={styles.priceWrapper}>${watch.price}</div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Link
+                to={constants.routes.UNDER_CONSTRUCTION}
+                style={styles.btnWrapper}
               >
-                <Link
-                  to={constants.routes.UNDER_CONSTRUCTION}
-                  style={styles.btnWrapper}
-                >
-                  <div style={styles.buyBtn}>{strings.buy}</div>
-                </Link>
-                <Link
-                  to={constants.routes.UNDER_CONSTRUCTION}
-                  style={styles.btnWrapper}
-                >
-                  <div style={styles.financeBtn}>{strings.finance}</div>
-                </Link>
-              </div>
+                <div style={styles.buyBtn}>{strings.buy}</div>
+              </Link>
+              <Link
+                to={constants.routes.UNDER_CONSTRUCTION}
+                style={styles.btnWrapper}
+              >
+                <div style={styles.financeBtn}>{strings.finance}</div>
+              </Link>
             </div>
-            <div style={styles.divider} />
-            <p>{watch.description}</p>
-            <div style={styles.specs}>
-              <div style={styles.properties}>
-                {caseKey}
-                {braceletKey}
-                {dialKey}
-                {diameterKey}
-                {movementKey}
-                {complicationsKey}
-              </div>
-              <div style={styles.values}>
-                <p>{watch.case}</p>
-                <p>{watch.bracelet}</p>
-                <p>{watch.dial}</p>
-                <p>{watch.diameter}</p>
-                <p>{watch.movement}</p>
-                <p>{complications}</p>
-              </div>
+          </div>
+          <div style={styles.divider} />
+          <p>{watch.description}</p>
+          <div style={styles.specs}>
+            <div style={styles.properties}>
+              {caseKey}
+              {braceletKey}
+              {dialKey}
+              {diameterKey}
+              {movementKey}
+              {complicationsKey}
+            </div>
+            <div style={styles.values}>
+              <p>{watch.case}</p>
+              <p>{watch.bracelet}</p>
+              <p>{watch.dial}</p>
+              <p>{watch.diameter}</p>
+              <p>{watch.movement}</p>
+              <p>{complications}</p>
             </div>
           </div>
         </div>
-        <div style={styles.others}>
-          {strings.otherWatchesBy}
-          {watch.brand}
-        </div>
-        <div style={styles.bottomWrapper}>
-          {this.state.otherWatches.length > 0
-            ? otherWatches
-            : activityIndicator}
-        </div>
       </div>
-    );
-  }
+      <div style={styles.others}>
+        {strings.otherWatchesBy}
+        {watch.brand}
+      </div>
+      <div style={styles.bottomWrapper}>
+        {otherWatches.length > 0 ? otherWatchesRendered : activityIndicator}
+      </div>
+    </div>
+  );
 }
-
-// export default class Product extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       watch: {},
-//       otherWatches: [],
-//       zoom: false,
-//     };
-//   }
-
-//   componentDidMount() {
-//     const { params } = this.props.match;
-//     axios
-//       .get(`${constants.api.WATCHES}/${params.brand}/${params._id}`)
-//       .then((res) => {
-//         this.setState({ watch: res.data });
-//         this.getOtherWatches();
-//       })
-//       .catch((err) => {
-//         console.log(`Error getting the product: ${err}`);
-//         window.location.href = "../../404";
-//         // dispatch({
-//         //   type: GET_ERRORS,
-//         //   payload: err.response.data,
-//         // });
-//       });
-//   }
-
-//   zoom = () => {
-//     this.setState({
-//       zoom: !this.state.zoom,
-//     });
-//   };
-
-//   getOtherWatches = () => {
-//     axios
-//       .get(`${constants.api.WATCHES}/${this.state.watch.brand}`)
-//       .then((res) => {
-//         this.setState({ otherWatches: res.data });
-//       })
-//       .catch((err) => {
-//         console.log(`error: ${err}`);
-//         // dispatch({
-//         //   type: GET_ERRORS,
-//         //   payload: err.response.data,
-//         // });
-//       });
-//   };
-
-//   render() {
-//     const { watch } = this.state;
-//     const watchImage = `data:image/jpeg;base64,${watch.image}`;
-//     const otherWatches = this.state.otherWatches
-//       .filter((product) => product.inStock)
-//       .filter((product) => product._id != watch._id)
-//       .map((watch, i) => {
-//         return <ProductPreview watch={watch} />;
-//       });
-//     const activityIndicator = (
-//       <div style={styles.activityIndicator}>
-//         <Dots />
-//       </div>
-//     );
-//     const imageFullSize = (
-//       <div style={styles.fullImageWrapper}>
-//         {/* Change this to an actual icon instead of just text */}
-//         <div style={styles.fullImageClose} onClick={this.zoom}>
-//           X
-//         </div>
-//         <img
-//           alt={strings.accessibility.fullImage}
-//           src={watchImage}
-//           style={{ maxHeight: "100%" }}
-//         />
-//       </div>
-//     );
-//     const caseKey = watch.case && <p>{strings.case}</p>;
-//     const braceletKey = watch.bracelet && <p>{strings.bracelet}</p>;
-//     const dialKey = watch.dial && <p>{strings.dial}</p>;
-//     const diameterKey = watch.diameter && <p>{strings.diameter}</p>;
-//     const movementKey = watch.movement && <p>{strings.movement}</p>;
-//     const complicationsKey = watch.complications !== undefined &&
-//       watch.complications.length > 0 && <p>{strings.complications}</p>;
-//     const complications =
-//       watch.complications !== undefined &&
-//       watch.complications.map((e, i) => {
-//         return i > 0 ? `, ${e}` : e;
-//       });
-
-//     return (
-//       <div style={styles.masterWrapper}>
-//         {this.state.zoom && imageFullSize}
-//         <div style={styles.topWrapper}>
-//           <div style={styles.imageWrapper}>
-//             <img
-//               alt={strings.accessibility.productImage}
-//               src={watchImage}
-//               style={styles.image}
-//               onClick={this.zoom}
-//             />
-//             <div style={styles.zoom}>
-//               {strings.clickToZoom}
-//               <img alt="" src={zoom} style={styles.zoomIcon} />
-//             </div>
-//           </div>
-//           <div style={styles.detailsWrapper}>
-//             <div style={styles.title}>{watch.brand}</div>
-//             <div style={styles.model}>{watch.model}</div>
-//             <div style={styles.buyWrapper}>
-//               <div style={styles.priceWrapper}>${watch.price}</div>
-//               <div
-//                 style={{
-//                   display: "flex",
-//                   justifyContent: "center",
-//                   alignItems: "center",
-//                 }}
-//               >
-//                 <Link
-//                   to={constants.routes.UNDER_CONSTRUCTION}
-//                   style={styles.btnWrapper}
-//                 >
-//                   <div style={styles.buyBtn}>{strings.buy}</div>
-//                 </Link>
-//                 <Link
-//                   to={constants.routes.UNDER_CONSTRUCTION}
-//                   style={styles.btnWrapper}
-//                 >
-//                   <div style={styles.financeBtn}>{strings.finance}</div>
-//                 </Link>
-//               </div>
-//             </div>
-//             <div style={styles.divider} />
-//             <p>{watch.description}</p>
-//             <div style={styles.specs}>
-//               <div style={styles.properties}>
-//                 {caseKey}
-//                 {braceletKey}
-//                 {dialKey}
-//                 {diameterKey}
-//                 {movementKey}
-//                 {complicationsKey}
-//               </div>
-//               <div style={styles.values}>
-//                 <p>{watch.case}</p>
-//                 <p>{watch.bracelet}</p>
-//                 <p>{watch.dial}</p>
-//                 <p>{watch.diameter}</p>
-//                 <p>{watch.movement}</p>
-//                 <p>{complications}</p>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//         <div style={styles.others}>
-//           {strings.otherWatchesBy}
-//           {watch.brand}
-//         </div>
-//         <div style={styles.bottomWrapper}>
-//           {this.state.otherWatches.length > 0
-//             ? otherWatches
-//             : activityIndicator}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
--- frontend/src/components/productPreview/ProductPreview.js
+++ frontend/src/components/productPreview/ProductPreview.js
@@ -4,50 +4,24 @@ import constants from "../../constants";
 import styles from "./styles";
 import strings from "./strings";
 
-export default class ProductPreview extends Component {
-  render() {
-    const { watch } = this.props;
-    return (
-      <div
-        style={{ textDecoration: "none", cursor: "pointer" }}
-        onClick={() =>
-          (window.location.href = `${constants.routes.PRODUCT}/${watch.brand}/${watch._id}`)
-        }
-      >
-        <div style={styles.itemWrapper}>
-          <img
-            style={styles.thumbnail}
-            src={`data:image/jpeg;base64,${watch.image}`}
-            alt={strings.accessibility.productImage}
-          />
-          <div>{watch.model}</div>
-          <div>${watch.price}</div>
-        </div>
+export default function ProductPreview(props) {
+  const { watch } = props;
+  return (
+    <div
+      style={{ textDecoration: "none", cursor: "pointer" }}
+      onClick={() =>
+        (window.location.href = `${constants.routes.PRODUCT}/${watch.brand}/${watch._id}`)
+      }
+    >
+      <div style={styles.itemWrapper}>
+        <img
+          style={styles.thumbnail}
+          src={`data:image/jpeg;base64,${watch.image}`}
+          alt={strings.accessibility.productImage}
+        />
+        <div>{watch.model}</div>
+        <div>${watch.price}</div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-// export default class ProductPreview extends Component {
-//   render() {
-//     const { watch } = this.props;
-//     return (
-//       <div
-//         style={{ textDecoration: "none", cursor: "pointer" }}
-//         onClick={() =>
-//           (window.location.href = `${constants.routes.PRODUCT}/${watch.brand}/${watch._id}`)
-//         }
-//       >
-//         <div style={styles.itemWrapper}>
-//           <img
-//             style={styles.thumbnail}
-//             src={`data:image/jpeg;base64,${watch.image}`}
-//             alt={strings.accessibility.productImage}
-//           />
-//           <div>{watch.model}</div>
-//           <div>${watch.price}</div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
